d78887864911a3c950bb8bb3bc9098b3ece2fc1a
Omar Alsabbagh <42855230+Omar-Alsabbagh17@users.noreply.github.com>
2022-05-06 23:47:04 -0700

    Create main.rs

---
+++ HW_2/rust/src/main.rs
@@ -0,0 +1,17 @@
+mod problem_2; 
+mod problem_1; 
+
+
+
+// ---
+
+fn main() {
+   
+
+    println!("Testing problem 1...");
+    problem_1::run();
+    println!("Testing problem 2....");
+    problem_2::run2();
+    println!("Done Testing")
+
+}
